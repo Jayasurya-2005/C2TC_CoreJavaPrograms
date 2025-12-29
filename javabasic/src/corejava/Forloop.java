fcbb6d5ab5c3f11b2a19c8625dbacf69b44ff5ea
jayas <jayas@surya>
2025-12-29 10:57:51 +0530

    commit

---
+++ javabasic/src/corejava/Forloop.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Forloop {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=1000 ; i++)   //(int i = 1000; i<=1;i--)
+		{	
+			System.out.println("Value of i:"+i);
+			//System.out.println(i);
+		}
+
+	}
+
+}
